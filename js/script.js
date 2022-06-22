cdd0c2bbbc0481962204e3b64caad093fa9093b5
joseotaviomendes123 <joseotaviomendesdacosta@gmail.com>
2022-06-21 22:29:15 -0300

    comitin de cada dia

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Criar uma função em JavaScript 
+//Formato ES6 baseado em Arrow Function
+
+//Função que realiza o cadastro de livros
+const cadastrar = () =>{
+
+    // Capitura o valor digitado no campo titulo
+    let titulo = document.getElementById('titulo')
+
+    alert(titulo)
+}
