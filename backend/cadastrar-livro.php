5f0e143e6997cd5a08b39fe73da0c96e180d46f1
joseotaviomendes123 <joseotaviomendesdacosta@gmail.com>
2022-06-23 22:27:48 -0300

    finalizado tb_livraria

--- backend/cadastrar-livro.php
+++ backend/cadastrar-livro.php
@@ -12,14 +12,42 @@ $valor = $_POST['valor'];
     $servidor = 'localhost';
     $usuario = 'root';
     $senha = '';
+    $bancodados = 'db_livraria';
+
+
 
     try {
-        $conexao= new PDO("mysql:host=$servidor;dbname=myDB", $usuario, $senha);
-        // set the PDO error mode to exception
+       // Definindo as configurações das configurações de conexão com o banco de dados
+        $conexao= new PDO("mysql:host=$servidor;dbname=$bancodados;charset=utf8", $usuario, $senha);
+        
+        // seta o modo de erro do PDO para exception
         $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
+
+        // query de inserção de dados no BD mySQL
+      $sql = "INSERT into 
+        tb_livros
+          (titulo,autor,valor,categoria)
+        VALUES 
+              ('$titulo','$autor','$valor','$categoria')"; 
+
+        // prepara a execução da query sql acima
+        $comando = $conexao->prepare($sql);
+
+        // executa a query preparada ascima
+        $comando->execute();
+
+        //criar um array para resposta ao usúario
+        $resposta = array("Resposta"=>"ok","mensagem"=>"Cadastro realizado com sucesso");
+
+      // converte o array resposta em JSON 
+      // JSON_UNESCAPED_UNICODE = Manter o arquivo com mapa de caracter padrão 
+      $json = json_encode($resposta,JSON_UNESCAPED_UNICODE);
+
+      echo $json;
+
+       // echo "Cadastro realizado com sucesso!";
       } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+        echo "Ti fude rapa: " . $e->getMessage();
       }
 
 // final da conexao
